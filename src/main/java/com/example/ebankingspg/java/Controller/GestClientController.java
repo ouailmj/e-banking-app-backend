2a9d7bab8484d9ef39fc78bf45b5aa0201b2d2e7
SalmaAlaoui <51100352+SalmaAlaoui@users.noreply.github.com>
2019-12-15 23:19:54 +0100

    modif dans fct create les gst
    hir cryptit les password b bcrypt et ajoutit les role a l'inscription des deux gestionnaire muhim dkchi f gestionnaireclient et manager controller

--- src/main/java/com/example/ebankingspg/java/Controller/GestClientController.java
+++ src/main/java/com/example/ebankingspg/java/Controller/GestClientController.java
@@ -1,49 +1,69 @@
 package com.example.ebankingspg.java.Controller;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import com.example.ebankingspg.java.Repository.AgenceRepository;
 import com.example.ebankingspg.java.Repository.ClientRepository;
 import com.example.ebankingspg.java.Repository.CommissionRepository;
 import com.example.ebankingspg.java.Repository.DeviseRepository;
+import com.example.ebankingspg.java.Repository.RoleRepository;
 import com.example.ebankingspg.java.response.StatistiqueResponse;
 import com.example.ebankingspg.java.services.GestClientService;
+import com.example.ebankingspg.java.services.RoleService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.ebankingspg.java.model.*;
-
 
 @CrossOrigin(origins = "*")
 @RestController
 public class GestClientController {
 
-    @Autowired
-    private GestClientService gestClientService;
+  @Autowired
+  private GestClientService gestClientService;
 
-    @Autowired
-    private AgenceRepository agenceRepository;
+  @Autowired
+  private AgenceRepository agenceRepository;
 
-    @Autowired
-    private ClientRepository clientRepository;
+  @Autowired
+  private ClientRepository clientRepository;
 
-    @Autowired
-    private DeviseRepository deviseRepository;
+  @Autowired
+  private DeviseRepository deviseRepository;
 
-    @Autowired
-    private CommissionRepository commissionRepository;
+  @Autowired
+  private CommissionRepository commissionRepository;
 
-    @GetMapping(produces = "application/json")
-    public List<GestClient> firstPage() {
-        List<GestClient> gestclients = gestClientService.findAll();
-        return gestclients;
-    }
+  @Autowired
+  private RoleRepository rolerep;
+
+  @GetMapping(produces = "application/json")
+  public List<GestClient> firstPage() {
+    List<GestClient> gestclients = gestClientService.findAll();
+    return gestclients;
+  }
 
 
-    @PostMapping
-    public GestClient create(@RequestBody GestClient gestcli) {
+  @Autowired
+  private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+  @PostMapping
+  public GestClient create(@RequestBody GestClient gestcli) {
+
+    String pass = gestcli.getPassword();
+    String newpass = bCryptPasswordEncoder.encode(pass);
+    gestcli.setPassword(newpass);
+    Set<Role> roles1 = new HashSet<Role>();
+    roles1.add(rolerep.findByRole("ROLE_CLIENT_MANAGER"));
+    gestcli.setRoles(roles1);
         gestClientService.create(gestcli);
         return gestcli;
     }
--- src/main/java/com/example/ebankingspg/java/Controller/GesttransacController.java
+++ src/main/java/com/example/ebankingspg/java/Controller/GesttransacController.java
@@ -1,10 +1,14 @@
 package com.example.ebankingspg.java.Controller;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ebankingspg.java.Repository.GestTransacRepository;
+import com.example.ebankingspg.java.Repository.RoleRepository;
 import com.example.ebankingspg.java.Repository.UserRepository;
 import com.example.ebankingspg.java.model.*;
 @CrossOrigin()
@@ -24,6 +29,8 @@ public class GesttransacController {
   @Autowired
    private GestTransacRepository gesttransrep;
 
+   @Autowired
+   private RoleRepository rolerep;
 
    @GetMapping(produces = "application/json")
    public List<GestTransac> firstPage() {
@@ -32,9 +39,18 @@ public class GesttransacController {
    }
 
 
+   @Autowired
+   private BCryptPasswordEncoder bCryptPasswordEncoder;
+
    @PostMapping
    public GestTransac create(@RequestBody GestTransac gestTransac) {
+    String pass = gestTransac.getPassword();
 
+    String newpass = bCryptPasswordEncoder.encode(pass);
+    gestTransac.setPassword(newpass);
+    Set<Role> roles1 = new HashSet<Role>();
+    roles1.add(rolerep.findByRole("ROLE_CLIENT_MANAGER"));
+    gestTransac.setRoles(roles1);
        gesttransrep.save(gestTransac);
 
        return gestTransac;
